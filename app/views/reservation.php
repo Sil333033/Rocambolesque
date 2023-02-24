164b8016527b6ff0f92afcd5c1ba8e7002dd5072
Claudiatjuhh <claudia.nijholt.2107@gmail.com>
2023-02-24 14:45:17 +0100

    reservation frontend start added

--- app/views/reservation.php
+++ app/views/reservation.php
@@ -8,7 +8,22 @@
     <title>Document</title>
 </head>
 <body>
-    
+<section class="reservation__section">
+        <div class="form">
+            <h2>RESERVEREN</h2>
+            <form action="">
+                <input type="text" placeholder="Enter the date and time">
+                <br>
+                <input type="text" placeholder="Enter your groupsize">
+                <br>
+                <input type="text" placeholder="Enter a description">
+            </form>
+        </div>
+        <div class="button-div">
+            <button class="custom-btn btn-2">
+                <a href="">RESERVEREN</a>
+            </button>
+        </div>
     <!-- Scripts -->
     <script src="https://kit.fontawesome.com/4227656b17.js" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
